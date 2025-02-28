5ef8957e5585891c20a4e187d6df00b875e4c254
Jack Sanderson <sandersonjack02@gmail.com>
2025-02-28 16:02:45 +0000

    added templates and static files

--- grid/apps.py
+++ grid/apps.py
@@ -6,12 +6,4 @@ class GridConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "grid"
 
-    def ready(self):
-        """Start the background task thread only in the main Gunicorn process"""
-        if os.environ.get("RUN_MAIN") == "true":  # Prevent multiple threads
-            from .tasks import background_scheduler
-            thread = threading.Thread(target=background_scheduler, daemon=True)
-            thread.start()
-            print("✅ Background thread started!")
-
     
